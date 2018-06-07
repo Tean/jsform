013dc2039e1acc6ed9a73ff26b348726aac4b545
netteans <netteans@gmail.com>
2018-06-07 17:04:10 +0800

    One Form need form.htmp subview.htmpl form.app.js form.event.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,36 @@
-$(document).ready(function () {
-    var app = new App();
-    $('.content').css('height', 9 * $(this).width() / 16);
-    $(window).resize(function (e) {
+$.getMultiScripts = function(arr, path) {
+    var _arr = $.map(arr, function(scr) {
+        return $.getScript( (path||"") + scr );
+    });
+
+    _arr.push($.Deferred(function( deferred ){
+        $( deferred.resolve );
+    }));
+
+    return $.when.apply($, _arr);
+};
+
+//your scripts
+var srcipts = [
+    'tmpl/form/form.app.js',
+    '/tmpl/form/form.event.js'
+];
+
+$.getMultiScripts(srcipts,'./js/').done(function () {
+    $(document).ready(function () {
+        var app = new App();
         $('.content').css('height', 9 * $(this).width() / 16);
-    });
-    $(document).on('click', '#testSpan', function () {
-        app.show();
-    });
-    $(document).on('click', '#test2Span', function () {
-        app2.test();
-    });
-    $(document).on('click', '#hideHeadSpan', function () {
-        app.hideHead();
+        $(window).resize(function (e) {
+            $('.content').css('height', 9 * $(this).width() / 16);
+        });
+        $(document).on('click', '#testSpan', function () {
+            app.show();
+        });
+        $(document).on('click', '#test2Span', function () {
+            app2.test();
+        });
+        $(document).on('click', '#hideHeadSpan', function () {
+            app.hideHead();
+        });
     });
 });
